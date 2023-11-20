aaa7b90d6322527f75e2a367ff0a1a378c5dc3bb
Nate Kootstra <30202083@student.publicboard.ca>
2023-11-20 18:54:19 -0500

    Created the setShuffleboardOutput() command. This allows for easily selecting what values should be displayed. Created Shuffleboard constants. Created getWheelEncoder(). It takes in a wheel ID, and returns a DoubleSupplier.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.SPI.Port;
 
@@ -18,13 +19,21 @@ import edu.wpi.first.wpilibj.SPI.Port;
  */
 public final class Constants {
   public static final class Motors {
-    public static final class IDs {
-      public static final int BACKLEFT = 0;
-      public static final int BACKRIGHT = 0;
-      public static final int FRONTLEFT = 0;
-      public static final int FRONTRIGHT = 0;
+    public static final class Wheels {
+      public static final class IDs {
+        public static final int BACKLEFT = 0;
+        public static final int BACKRIGHT = 0;
+        public static final int FRONTLEFT = 0;
+        public static final int FRONTRIGHT = 0;
+      }
+  
+      public static final NeutralMode[] NEUTRALMODES = {NeutralMode.Coast, NeutralMode.Brake};
+      public static final WPI_TalonSRX[] WHEELS = {new WPI_TalonSRX(Constants.Motors.Wheels.IDs.BACKLEFT), new WPI_TalonSRX(Constants.Motors.Wheels.IDs.BACKRIGHT), new WPI_TalonSRX(Constants.Motors.Wheels.IDs.FRONTLEFT), new WPI_TalonSRX(Constants.Motors.Wheels.IDs.FRONTRIGHT)};
     }
+    
+  }
 
-    public static final NeutralMode[] NEUTRALMODES = {NeutralMode.Coast, NeutralMode.Brake};
+  public static final class Shuffleboard {
+    public static final String  MAINTAB = "Debug";
   }
 }
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems;
 
+import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
@@ -11,15 +14,16 @@ import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class DriveSubsystem extends SubsystemBase {
   
-  private WPI_TalonSRX backLeftWheel = new WPI_TalonSRX(Constants.Motors.IDs.BACKLEFT);
-  private WPI_TalonSRX backRightWheel = new WPI_TalonSRX(Constants.Motors.IDs.BACKRIGHT);
-  private WPI_TalonSRX frontLeftWheel = new WPI_TalonSRX(Constants.Motors.IDs.FRONTLEFT);
-  private WPI_TalonSRX frontRightWheel = new WPI_TalonSRX(Constants.Motors.IDs.FRONTRIGHT);
+  private WPI_TalonSRX backLeftWheel = new WPI_TalonSRX(Constants.Motors.Wheels.IDs.BACKLEFT);
+  private WPI_TalonSRX backRightWheel = new WPI_TalonSRX(Constants.Motors.Wheels.IDs.BACKRIGHT);
+  private WPI_TalonSRX frontLeftWheel = new WPI_TalonSRX(Constants.Motors.Wheels.IDs.FRONTLEFT);
+  private WPI_TalonSRX frontRightWheel = new WPI_TalonSRX(Constants.Motors.Wheels.IDs.FRONTRIGHT);
   private MotorControllerGroup leftWheels = new MotorControllerGroup(frontLeftWheel, backLeftWheel);
   private MotorControllerGroup rightWheels = new MotorControllerGroup(frontRightWheel, backRightWheel);
   private DifferentialDrive wheels = new DifferentialDrive(leftWheels, rightWheels);
@@ -27,23 +31,28 @@ public class DriveSubsystem extends SubsystemBase {
   private Gyro gyro = new ADXRS450_Gyro();
 
 
-  
+
   public DriveSubsystem() {
 
     setInverted(false, false, false, false);
     setBrake(true);
     setSafety(false);
+
+    setShuffleboardOutput(true, true, false, false, true, false);
+
   }
+
+
 
   public void setSafety(boolean safe){
     wheels.setSafetyEnabled(safe);
   }
 
   public void setBrake(boolean brake){
-    backLeftWheel.setNeutralMode(Constants.Motors.NEUTRALMODES[brake ? 1 : 0]);
-    backRightWheel.setNeutralMode(Constants.Motors.NEUTRALMODES[brake ? 1 : 0]);
-    frontLeftWheel.setNeutralMode(Constants.Motors.NEUTRALMODES[brake ? 1 : 0]);
-    frontRightWheel.setNeutralMode(Constants.Motors.NEUTRALMODES[brake ? 1 : 0]);
+    backLeftWheel.setNeutralMode(Constants.Motors.Wheels.NEUTRALMODES[brake ? 1 : 0]);
+    backRightWheel.setNeutralMode(Constants.Motors.Wheels.NEUTRALMODES[brake ? 1 : 0]);
+    frontLeftWheel.setNeutralMode(Constants.Motors.Wheels.NEUTRALMODES[brake ? 1 : 0]);
+    frontRightWheel.setNeutralMode(Constants.Motors.Wheels.NEUTRALMODES[brake ? 1 : 0]);
   }
 
   public void setInverted(boolean backLeft, boolean backRight, boolean frontLeft, boolean frontRight){
@@ -53,13 +62,35 @@ public class DriveSubsystem extends SubsystemBase {
     frontRightWheel.setInverted(frontRight);
   }
 
+  public void setShuffleboardOutput(boolean gyro, boolean safetyEnabled, boolean backLeftWheel, boolean backRightWheel, boolean frontLeftWheel, boolean frontRightWheel){
+    if(gyro)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addDouble("Gyro", getGyro());
+    if(safetyEnabled)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addBoolean("Safety Enabled", getSafetyEnabled());
+    if(backLeftWheel)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addDouble("Back Left Wheel", getWheelEncoder(Constants.Motors.Wheels.IDs.BACKLEFT));
+    if(backRightWheel)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addDouble("Back Right Wheel", getWheelEncoder(Constants.Motors.Wheels.IDs.BACKRIGHT));
+    if(frontLeftWheel)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addDouble("Front Left Wheel", getWheelEncoder(Constants.Motors.Wheels.IDs.FRONTLEFT));
+    if(frontRightWheel)
+      Shuffleboard.getTab(Constants.Shuffleboard.MAINTAB).addDouble("Front Right Wheel", getWheelEncoder(Constants.Motors.Wheels.IDs.FRONTRIGHT));
+  }
 
   public void resetGyro(){
     gyro.reset();
   }
 
-  public double getGyro(){
-    return gyro.getAngle();
+  public DoubleSupplier getGyro(){
+    return () -> gyro.getAngle();
+  }
+
+  public DoubleSupplier getWheelEncoder(int wheelID){
+    return () -> Constants.Motors.Wheels.WHEELS[wheelID].getSelectedSensorPosition();
+  }
+
+  public BooleanSupplier getSafetyEnabled(){
+    return () -> (backLeftWheel.isSafetyEnabled());
   }
 
   public void arcadeDrive(double speed, double rotation){
